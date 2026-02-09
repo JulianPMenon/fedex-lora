bf3682cbc570f0c9170eeddc33accca709d9eb45
JulianPMenon <97676461+JulianPMenon@users.noreply.github.com>
2026-02-09 16:51:04 +0100

    add newer slum file

--- slurm_fedvera.sh
+++ slurm_fedvera.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=12G
-#SBATCH --time=08:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mail-type=ALL 
 #SBATCH --gpus=1
 
@@ -37,8 +37,11 @@ EOF
 echo "============================"
 
 # Run your FedVera script for this client
-python fed_train_glue.py --model=models/models--FacebookAI--roberta-base/snapshots/e2da8e2f811d1448a5b465c236feacd80ffbac7b --task=cola --agg_type=ours_vera --vera --num_clients=3 --r=120 --rounds=3 --lr=1e-3 --local_epochs=80 
+# FedEX VeRA
+# python fed_train_glue.py --model=models/models--FacebookAI--roberta-base/snapshots/e2da8e2f811d1448a5b465c236feacd80ffbac7b --task=cola --agg_type=ours_vera --vera --num_clients=3 --r=120 --rounds=3 --lr=1e-3 --local_epochs=80
+# Fedaverage with Vera
+# python fed_train_glue.py --model=models/models--FacebookAI--roberta-base/snapshots/e2da8e2f811d1448a5b465c236feacd80ffbac7b --task=cola --agg_type=ffa --vera --num_clients=3 --r=120 --rounds=3 --lr=1e-3 --local_epochs=80
 # Run your FedEXLora script for this client
-# python fed_train_glue.py --model=models/models--FacebookAI--roberta-base/snapshots/e2da8e2f811d1448a5b465c236feacd80ffbac7b --task=cola --agg_type=ours --num_clients=3 --r=6 --rounds=3 --lr=1e-3 --local_epochs=80  
+python fed_train_glue.py --model=models/models--FacebookAI--roberta-base/snapshots/e2da8e2f811d1448a5b465c236feacd80ffbac7b --task=mrpc --agg_type=ours --num_clients=3 --r=6 --rounds=3 --lr=1e-3 --local_epochs=80  
 # Or, if you use a different entry point:
 # python fed_agg.py --client_id $CLIENT_ID
